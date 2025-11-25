4f0b71b727dd325b016ee0639c60f4d493bcbf65
Priyanka Srivastava <srivastavapriyanka71307@gmail.com>
2025-11-25 23:02:22 +0530

    Object of array solving by using filter,sort,map

---
+++ Assignment/HigherOrderFunction/objectFunction.js
@@ -0,0 +1,25 @@
+const students =  [{ "name": "Alice", "marks": 58 },
+  { "name": "Bob", "marks": 85 },
+  { "name": "Charlie", "marks": 92 },
+  { "name": "David", "marks": 45 },
+  { "name": "Emma", "marks": 76 },
+  { "name": "Frank", "marks": 63 },
+  { "name": "Grace", "marks": 89 },
+  { "name": "Hannah", "marks": 95 },
+  { "name": "Ian", "marks": 54 },
+  { "name": "Jack", "marks": 79 },
+  { "name": "Kate", "marks": 67 },
+  { "name": "Leo", "marks": 88 },
+  { "name": "Mia", "marks": 91 },
+  { "name": "Nathan", "marks": 72 },
+  { "name": "Olivia", "marks": 82 }
+]
+function processStudents(){
+const passedStudent = students.filter(Student =>Student.marks > 60)
+
+ passedStudent.sort((a,b)=> b.marks - a.marks)
+
+const sortedNames = passedStudent.map(studentss => studentss.name)
+return sortedNames
+}
+console.log(processStudents(students));
